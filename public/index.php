7ce63c8ce2a9e0d564949305ba9b989eb4c697c2
shohsanam <info@app.uz>
2025-04-01 02:16:38 +0500

    4 commit

--- public/index.php
+++ public/index.php
@@ -1,10 +1,25 @@
 <?php
+declare(strict_types=1);
+
+use Dotenv\Dotenv;
 
 try {
-    require_once('../vendor/autoload.php');
+    require_once dirname(__DIR__).'/App/Config/Core.php';
+    require_once dirname(__DIR__).'/vendor/autoload.php';
 
-    echo time();
 
+
+    /**
+     * Настройки окружения
+     * .env-файле.
+     * ENV=development / production
+     */
+    $dotenv = Dotenv::createImmutable(__DIR__ . '/../');
+    $dotenv->load();
+
+    $env = $_ENV['APP_ENV'] ?: 'production';
+
+    echo $env;
 
 } catch (Exception $e) {
     echo $e->getMessage();
